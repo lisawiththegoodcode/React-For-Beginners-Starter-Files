a4646679e13f6bd8abde1c969b0e4b80c6d520e3
lisa umhey <lisa.umhey@gmail.com>
2019-02-12 13:29:18 -0600

    add fish form complete, state updating

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,14 +5,30 @@ import Inventory from "./Inventory";
 
 
 class App extends React.Component{
+    state = {
+        fishes: {},
+        order: {}
+    };
+    addFish = fish =>{
+        console.log("adding a fish");
+        //1. take a copy of the existing state
+        const fishes = {...this.state.fishes};
+        //2. add new fish to that fishes varuable
+        fishes[`fish${Date.now()}`] = fish;
+        //3. set new fishes object to state
+        this.setState({
+            fishes: fishes
+        });
+        //alternatively this.setState({fishes}); will work if variable names match
+    };
     render() {
-        return (
+        return ( 
             <div className="catch-of-the-day">
                <div className="menu">
                     <Header tagline="Fresh Seafood Market"/>
                 </div>
-                <Inventory />
                 <Order />
+                <Inventory addFish={this.addFish}/>
             </div>
         );
     }
